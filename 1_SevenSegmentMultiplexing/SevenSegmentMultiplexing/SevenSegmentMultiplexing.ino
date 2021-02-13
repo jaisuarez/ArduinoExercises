530d7b4d91ecf0d2425e949eba682a3f9b7e26fc
Jazer Marie Suarez <jazermariesuarez@gmail.com>
2021-02-13 18:34:14 +0800

    Update definitions

--- 1_SevenSegmentMultiplexing/SevenSegmentMultiplexing/SevenSegmentMultiplexing.ino
+++ 1_SevenSegmentMultiplexing/SevenSegmentMultiplexing/SevenSegmentMultiplexing.ino
@@ -1,90 +1,80 @@
-#define a 7
-#define b 6
-#define c 5
-#define d 4
-#define e 3
-#define f 2
-#define g 1
-#define p 0
-#define d1 A0
-#define d2 A1
-#define d3 A2
-#define d4 A3
-#define del 5
+#define SEG_A 7
+#define SEG_B 6
+#define SEG_C 5
+#define SEG_D 4
+#define SEG_E 3
+#define SEG_F 2
+#define SEG_G 1
+#define SEG_P 0
+#define D1 A0
+#define D2 A1
+#define D3 A2
+#define D4 A3
+#define DELAY_5MS 5
 
 float n = 0;
-int count = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(a, OUTPUT);
-  pinMode(b, OUTPUT);
-  pinMode(c, OUTPUT);
-  pinMode(d, OUTPUT);
-  pinMode(e, OUTPUT);
-  pinMode(f, OUTPUT);
-  pinMode(g, OUTPUT);
-  pinMode(p, OUTPUT);
-  pinMode(d1, OUTPUT);
-  pinMode(d2, OUTPUT);
-  pinMode(d3, OUTPUT);
-  pinMode(d4, OUTPUT);
-
-  // Timer0 is already used for millis() - we'll just interrupt somewhere
-  // in the middle and call the "Compare A" function below
-  OCR0A = 0xAF;
-  TIMSK0 |= _BV(OCIE0A);
+  pinMode(SEG_A, OUTPUT);
+  pinMode(SEG_B, OUTPUT);
+  pinMode(SEG_C, OUTPUT);
+  pinMode(SEG_D, OUTPUT);
+  pinMode(SEG_E, OUTPUT);
+  pinMode(SEG_F, OUTPUT);
+  pinMode(SEG_G, OUTPUT);
+  pinMode(SEG_P, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
 }
 
 void loop() {
   clearLEDs();//clear the 7-segment display screen
   pickDigit(0);//Light up 7-segment display d1
   pickNumber((int)n/1000);// get the value of thousand
-  delay(5);//delay 5ms
+  delay(DELAY_5MS);
 
   clearLEDs();//clear the 7-segment display screen
   pickDigit(1);//Light up 7-segment display d1
   pickNumber(((int)n%1000)/100);// get the value of thousand
-  delay(5);//delay 5ms
+  delay(DELAY_5MS);
 
   clearLEDs();//clear the 7-segment display screen
   pickDigit(2);//Light up 7-segment display d1
   pickNumber(((int)n%100)/10);// get the value of thousand
-  delay(5);//delay 5ms
+  delay(DELAY_5MS);
 
   clearLEDs();//clear the 7-segment display screen
   pickDigit(3);//Light up 7-segment display d1
   pickNumber((int)n%10);// get the value of thousand
-  delay(5);//delay 5ms
-}
+  delay(DELAY_5MS);
 
-// Interrupt is called once a millisecond, 
-SIGNAL(TIMER0_COMPA_vect) 
-{
-  n+=0.001;
+  n+=0.020;
 }
 
 void pickDigit(int x) //light up a 7-segment display
 {
   //The 7-segment LED display is a common-cathode one. So also use digitalWrite to  set d1 as high and the LED will go out
-  digitalWrite(d1, LOW);
-  digitalWrite(d2, LOW);
-  digitalWrite(d3, LOW);
-  digitalWrite(d4, LOW);
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, LOW);
+  digitalWrite(D3, LOW);
+  digitalWrite(D4, LOW);
 
   switch(x)
   {
     case 0: 
-    digitalWrite(d1, HIGH);//Light d1 up 
+    digitalWrite(D1, HIGH);//Light d1 up 
     break;
     case 1: 
-    digitalWrite(d2, HIGH); //Light d2 up 
+    digitalWrite(D2, HIGH); //Light d2 up 
     break;
     case 2: 
-    digitalWrite(d3, HIGH); //Light d3 up 
+    digitalWrite(D3, HIGH); //Light d3 up 
     break;
     default: 
-    digitalWrite(d4, HIGH); //Light d4 up 
+    digitalWrite(D4, HIGH); //Light d4 up 
     break;
   }
 }
@@ -128,121 +118,121 @@ void pickNumber(int x)
 
 void clearLEDs() //clear the 7-segment display screen
 {
-  digitalWrite(a, HIGH);
-  digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
-  digitalWrite(d, HIGH);
-  digitalWrite(e, HIGH);
-  digitalWrite(f, HIGH);
-  digitalWrite(g, HIGH);
-  digitalWrite(p, HIGH);
+  digitalWrite(SEG_A, HIGH);
+  digitalWrite(SEG_B, HIGH);
+  digitalWrite(SEG_C, HIGH);
+  digitalWrite(SEG_D, HIGH);
+  digitalWrite(SEG_E, HIGH);
+  digitalWrite(SEG_F, HIGH);
+  digitalWrite(SEG_G, HIGH);
+  digitalWrite(SEG_P, HIGH);
 }
 
 void zero()
 {
-  digitalWrite(a, LOW);
-  digitalWrite(b, LOW);
-  digitalWrite(c, LOW);
-  digitalWrite(d, LOW);
-  digitalWrite(e, LOW);
-  digitalWrite(f, LOW);
-  digitalWrite(g, HIGH);
+  digitalWrite(SEG_A, LOW);
+  digitalWrite(SEG_B, LOW);
+  digitalWrite(SEG_C, LOW);
+  digitalWrite(SEG_D, LOW);
+  digitalWrite(SEG_E, LOW);
+  digitalWrite(SEG_F, LOW);
+  digitalWrite(SEG_G, HIGH);
 }
 
 void one()
 {
-  digitalWrite(a, HIGH);
-  digitalWrite(b, LOW);
-  digitalWrite(c, LOW);
-  digitalWrite(d, HIGH);
-  digitalWrite(e, HIGH);
-  digitalWrite(f, HIGH);
-  digitalWrite(g, HIGH);
+  digitalWrite(SEG_A, HIGH);
+  digitalWrite(SEG_B, LOW);
+  digitalWrite(SEG_C, LOW);
+  digitalWrite(SEG_D, HIGH);
+  digitalWrite(SEG_E, HIGH);
+  digitalWrite(SEG_F, HIGH);
+  digitalWrite(SEG_G, HIGH);
 }
 
 void two()
 {
-  digitalWrite(a, LOW);
-  digitalWrite(b, LOW);
-  digitalWrite(c, HIGH);
-  digitalWrite(d, LOW);
-  digitalWrite(e, LOW);
-  digitalWrite(f, HIGH);
-  digitalWrite(g, LOW);
+  digitalWrite(SEG_A, LOW);
+  digitalWrite(SEG_B, LOW);
+  digitalWrite(SEG_C, HIGH);
+  digitalWrite(SEG_D, LOW);
+  digitalWrite(SEG_E, LOW);
+  digitalWrite(SEG_F, HIGH);
+  digitalWrite(SEG_G, LOW);
 }
 void three()
 {
-  digitalWrite(a, LOW);
-  digitalWrite(b, LOW);
-  digitalWrite(c, LOW);
-  digitalWrite(d, LOW);
-  digitalWrite(e, HIGH);
-  digitalWrite(f, HIGH);
-  digitalWrite(g, LOW);
+  digitalWrite(SEG_A, LOW);
+  digitalWrite(SEG_B, LOW);
+  digitalWrite(SEG_C, LOW);
+  digitalWrite(SEG_D, LOW);
+  digitalWrite(SEG_E, HIGH);
+  digitalWrite(SEG_F, HIGH);
+  digitalWrite(SEG_G, LOW);
 }
 
 void four()
 {
-  digitalWrite(a, HIGH);
-  digitalWrite(b, LOW);
-  digitalWrite(c, LOW);
-  digitalWrite(d, HIGH);
-  digitalWrite(e, HIGH);
-  digitalWrite(f, LOW);
-  digitalWrite(g, LOW);
+  digitalWrite(SEG_A, HIGH);
+  digitalWrite(SEG_B, LOW);
+  digitalWrite(SEG_C, LOW);
+  digitalWrite(SEG_D, HIGH);
+  digitalWrite(SEG_E, HIGH);
+  digitalWrite(SEG_F, LOW);
+  digitalWrite(SEG_G, LOW);
 }
 
 void five()
 {
-  digitalWrite(a, LOW);
-  digitalWrite(b, HIGH);
-  digitalWrite(c, LOW);
-  digitalWrite(d, LOW);
-  digitalWrite(e, HIGH);
-  digitalWrite(f, LOW);
-  digitalWrite(g, LOW);
+  digitalWrite(SEG_A, LOW);
+  digitalWrite(SEG_B, HIGH);
+  digitalWrite(SEG_C, LOW);
+  digitalWrite(SEG_D, LOW);
+  digitalWrite(SEG_E, HIGH);
+  digitalWrite(SEG_F, LOW);
+  digitalWrite(SEG_G, LOW);
 }
 
 void six()
 {
-  digitalWrite(a, LOW);
-  digitalWrite(b, HIGH);
-  digitalWrite(c, LOW);
-  digitalWrite(d, LOW);
-  digitalWrite(e, LOW);
-  digitalWrite(f, LOW);
-  digitalWrite(g, LOW);
+  digitalWrite(SEG_A, LOW);
+  digitalWrite(SEG_B, HIGH);
+  digitalWrite(SEG_C, LOW);
+  digitalWrite(SEG_D, LOW);
+  digitalWrite(SEG_E, LOW);
+  digitalWrite(SEG_F, LOW);
+  digitalWrite(SEG_G, LOW);
 }
 
 void seven()
 {
-  digitalWrite(a, LOW);
-  digitalWrite(b, LOW);
-  digitalWrite(c, LOW);
-  digitalWrite(d, HIGH);
-  digitalWrite(e, HIGH);
-  digitalWrite(f, HIGH);
-  digitalWrite(g, HIGH);
+  digitalWrite(SEG_A, LOW);
+  digitalWrite(SEG_B, LOW);
+  digitalWrite(SEG_C, LOW);
+  digitalWrite(SEG_D, HIGH);
+  digitalWrite(SEG_E, HIGH);
+  digitalWrite(SEG_F, HIGH);
+  digitalWrite(SEG_G, HIGH);
 }
 
 void eight()
 {
-  digitalWrite(a, LOW);
-  digitalWrite(b, LOW);
-  digitalWrite(c, LOW);
-  digitalWrite(d, LOW);
-  digitalWrite(e, LOW);
-  digitalWrite(f, LOW);
-  digitalWrite(g, LOW);
+  digitalWrite(SEG_A, LOW);
+  digitalWrite(SEG_B, LOW);
+  digitalWrite(SEG_C, LOW);
+  digitalWrite(SEG_D, LOW);
+  digitalWrite(SEG_E, LOW);
+  digitalWrite(SEG_F, LOW);
+  digitalWrite(SEG_G, LOW);
 }
 
 void nine()
 {
-  digitalWrite(a, LOW);
-  digitalWrite(b, LOW);
-  digitalWrite(c, LOW);
-  digitalWrite(d, LOW);
-  digitalWrite(e, HIGH);
-  digitalWrite(f, LOW);
-  digitalWrite(g, LOW);
+  digitalWrite(SEG_A, LOW);
+  digitalWrite(SEG_B, LOW);
+  digitalWrite(SEG_C, LOW);
+  digitalWrite(SEG_D, LOW);
+  digitalWrite(SEG_E, HIGH);
+  digitalWrite(SEG_F, LOW);
+  digitalWrite(SEG_G, LOW);
 }
